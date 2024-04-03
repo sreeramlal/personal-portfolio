3f286d19ce485917edbcef74dfdb574f878dc9e3
sreeramlal <sreeramlalvp123@gmail.com>
2024-04-03 12:53:25 +0530

    updated bottom link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,10 +11,11 @@ function Footer() {
       <nav>
         <div className="nav-links-container">
           <ul className="nav-links">
+          <li><a href="#desktop-nav">Home</a></li>
+
             <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
             {/* <li><a href="#projects">Projects</a></li> */}
-            <li><a href="#contact">Contact</a></li>
           </ul>
         </div>
       </nav>
